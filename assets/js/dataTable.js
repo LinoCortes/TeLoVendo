6ce805afe766f0d6ec58683a4ceacea127309812
Sergio_Antonio_Morales <s.morales.jor@gmail.com>
2023-04-03 12:48:11 -0400

    se agrega container en la tabla

---
+++ assets/js/dataTable.js
@@ -0,0 +1,7 @@
+//datatable
+$(document).ready( function () {
+    $('#myTable').DataTable();
+});
+
+//inicia datatables
+let table = new DataTable('#myTable');
--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -49,9 +49,3 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
 
-  //datatable
-  $(document).ready( function () {
-    $('#myTable').DataTable();
-} );
-//inicia datatables
-let table = new DataTable('#myTable');
